327844f044ba28cfb223ed0ee1cbf03ad78c4c9a
edcarter <edcarter@ualberta.ca>
2015-10-17 17:01:17 -0600

    added non compiling test cases

--- ReCarded/app/src/androidTest/java/com/sherpasteven/sscte/UseCase4Test.java
+++ ReCarded/app/src/androidTest/java/com/sherpasteven/sscte/UseCase4Test.java
@@ -10,4 +10,341 @@ public class UseCase4Test extends ApplicationTestCase<Application> {
     public UseCase4Test(Class<Application> applicationClass) {
         super(applicationClass);
     }
+
+    //Use Case Name: A Borrower trades with a friend
+//Participating Actors: Borrower, Owner, Inventory, Trade
+//Goal: A borrower can trade 0 to many cards for 0 to many cards that are owned by the
+    borrowers friend. The friend can either accept or decline the trade.
+    //Trigger: Borrower clicks the trade button to initiate a trade with a friend.
+//Precondition: Borrower has a friend in which the borrower can trade with.
+//Postcondition: Borrower has sent the trade request to the borrower’s friend
+    and the friend has accepted the trade.
+//Related User Stories: US04.01.01 - US04.07.01
+
+    //Basic Flow
+// 1)Borrower click the trade button and chooses a friend to trade with.
+// 2)Borrower adds and removes 0 to many cards that they are willing to trade.
+// 3)Borrower adds and removes 0 to many cards that they want from their friend.
+// 4)Borrower presses the send trade button to send the trade notification,
+    to their friend.
+    // 5)Owner gets the trade notification and opens it up.
+// 6)Owner accepts trade.
+// 7)Email opens up for the owner.
+// 7)Owner adds comments to send to the borrower telling that person how
+    they will be able to go about doing the trade.
+// 8)An email is sent to the Owner and the Borrower with the trade information.
+
+    //Exceptions
+// 2) User decides to stop the trade.
+// 2.1) User presses the back button. The state will not be saved.
+// 2.2) The system exits the use case
+// 3) User decides to stop the trade.
+// 3.1) User presses the back button. The state will not be saved.
+// 3.2) The system exits the use case
+// 6.1) Owner presses the decline button to decline the trade
+// 6.1.1) The app will remove the notification and save the trade in the
+    trades list.
+    // 6.1.2) The system will exit the use case.
+// 6.2) Owner presses the counter offer button to offer another trade
+// 6.2.1) System resets to step 2, except the Owner will become the Borrower
+    and the Borrower will become the Owner.
+
+// US04.01.01
+// Borrower wants to trade a card with the owner.
+// Send a trade request to an owner of a card and check if the owner has that request.
+
+    void testUS040101 () {
+        User borrower = new User(currentUser.Name, “Canada”);
+        User owner = new User(“Mr. Bean”, “Canada”);
+
+        // Borrower adds cards with the “plus” button.
+        Trade trade = new Trade(borrower, owner);
+        trade.list1.add(borrower.getInventory().getCard(“Darkrai”);
+        trade.list2.add(owner.getInventory().getCard(“Mewtwo”);
+
+        // Send trade offer button clicked.
+        trade.sendTrade(owner);
+        trade.setNotification(owner);
+        assertTrue(owner.getTrades().hasTrade(trade));
+    }
+
+// US04.02.01
+// Test to see if the owner has a trade notification.
+
+    void testUS040201 () {
+        User borrower = new User(currentUser.Name, “Canada”);
+        User owner = new User(“Mr. Bean”, “Canada”);
+
+        // Borrower adds cards with the “plus” button.
+        Trade trade = new Trade(borrower, owner);
+        trade.list1.add(borrower.getInventory().getCard(“Darkrai”);
+        trade.list2.add(owner.getInventory().getCard(“Mewtwo”);
+
+        // Send trade offer button clicked.
+        trade.sendTrade(owner);
+        trade.setNotification(owner);
+
+        assertTrue(owner.getNotifications().contains(trade.setNotification(owner)));
+    }
+
+// US04.03.01
+// Owner can accept of decline notification.
+
+    void testUS040301 () {
+        User borrower = new User(currentUser.Name, “Canada”);
+        User owner = new User(“Mr. Bean”, “Canada”);
+
+        // Borrower adds cards with the “plus” button.
+        Trade trade1 = new Trade(borrower, owner);
+        trade1.list1.add(borrower.getInventory().getCard(“Darkrai”);
+        trade1.list2.add(owner.getInventory().getCard(“Mewtwo”);
+
+        // Borrower adds cards with the “plus” button.
+        Trade trade2 = new Trade(borrower, owner);
+        trade2.list1.add(borrower.getInventory().getCard(“Darkrai”);
+        trade2.list2.add(owner.getInventory().getCard(“Mewtwo”);
+
+        // Send trade offer button clicked.
+        trade1.sendTrade(owner);
+        trade1.setNotification(owner);
+        trade2.sendTrade(owner);
+        trade2.setNotification(owner);
+
+        //Owner clicks the accept button
+        assertTrue(trade.1status() == “ACCEPTED”);
+
+        //Owner clicks the decline button
+        assertFalse(trade2.status() == “DECLINED”);
+    }
+
+// US04.04.01
+// Owner can set a counter offer if the owner declines a trade
+
+    void testUS040401 () {
+        User borrower = new User(currentUser.Name, “Canada”);
+        User owner = new User(“Mr. Bean”, “Canada”);
+
+        // Borrower adds cards with the “plus” button.
+        Trade trade = new Trade(borrower, owner);
+        trade.list1.add(borrower.getInventory().getCard(“Darkrai”);
+        trade.list2.add(owner.getInventory().getCard(“Mewtwo”);
+
+        // Send trade offer button clicked.
+        trade.sendTrade(owner);
+        trade.setNotification(owner);
+
+        // Owner declines the trade.
+        // Owner makes a new trade offer when the counter offer button is clicked.
+        if(trade.status() == “DECLINED”) {
+            Trade trade2 = newTrade(owner, borrower);
+            trade2.list1.add(owner.getInventory().getCard(“Mewtwo”);
+            trade2.list2.add(borrower.getInventory().getCard(“Mew”);
+
+            trade2.sendTrade(owner);
+            trade2.setNotification(borrower);
+        }
+
+        assertTrue(borrower.getNotifications().contains(trade2.setNotification(borrower)));
+    }
+
+// US04.05.01
+// The user or borrower can edit the trade or counter-trade.
+
+    void testUS040501 () {
+        User borrower = new User(currentUser.Name, “Canada”);
+        User owner = new User(“Mr. Bean”, “Canada”);
+
+        // Borrower adds and removes cards with the plus and minus button.
+        Trade trade = new Trade(borrower, owner);
+        trade.list1.add(borrower.getInventory().getCard(“Darkrai”);
+        trade.list1.add(borrower.getInventory().getCard(“Mew”);
+        trade.list2.add(owner.getInventory().getCard(“Mewtwo”);
+        trade.list2.remove(1);
+
+        assertTrue(trade.list1.contains(“Darkrai”));
+        assertTrue(trade.list2.contains(“Mewtwo”));
+        assertFalse(trade.list1.contains(“Mew”));
+
+        // Send trade offer button clicked.
+        trade.sendTrade(owner);
+        trade.setNotification(owner);
+
+        // Owner declines the trade.
+        // Owner makes a new trade offer when the counter offer button is clicked.
+        if(trade.status() == “DECLINED”) {
+            // Owner adds and removes cards with the plus and minus button.
+            Trade trade2 = newTrade(owner, borrower);
+            trade2.list1.add(owner.getInventory().getCard(“Deoxys”);
+            trade2.list1.add(owner.getInventory().getCard(“Register”);
+            trade2.list1.remove(1);
+            trade2.list2.add(borrower.getInventory().getCard(“Mew”);
+
+            assertTrue(trade.list1.contains(“Deoxys”));
+            assertTrue(trade.list2.contains(“Mew”));
+            assertFalse(trade.list1.contains(“Register”));
+
+            trade2.sendTrade(owner);
+            trade2.setNotification(borrower);
+        }
+    }
+
+// US04.06.01
+// The borrower can delete the trade or counter-trade.
+
+    void testUS040601() {
+        User borrower = new User(currentUser.Name, “Canada”);
+        User owner = new User(“Mr. Bean”, “Canada”);
+
+        // Borrower adds and removes cards with the plus and minus button.
+        Trade trade = new Trade(borrower, owner);
+        trade.list1.add(borrower.getInventory().getCard(“Darkrai”);
+        trade.list2.add(owner.getInventory().getCard(“Mewtwo”);
+
+        // Send trade offer button clicked.
+        trade.sendTrade(owner);
+        trade.setNotification(owner);
+
+        // Owner declines the trade.
+        // Owner makes a new trade offer when the counter offer button is clicked.
+        if(trade.status() == “DECLINED”) {
+            // Owner adds and removes cards with the plus and minus button.
+            Trade trade2 = newTrade(owner, borrower);
+            trade2.list1.add(owner.getInventory().getCard(“Deoxys”);
+            trade2.list2.add(borrower.getInventory().getCard(“Mew”);
+
+            // User clicks the delete trade button or back
+            // button to stop the trade they are making.
+            trade.destructor();
+        }
+
+        assertNull(trade);
+
+        User borrower2 = new User(currentUser.Name, “Canada”);
+        User owner2 = new User(“Mr. Bean”, “Canada”);
+
+        // Borrower adds and removes cards with the plus and minus button.
+        Trade trade2 = new Trade(borrower2, owner2);
+        trade2.list1.add(borrower.getInventory().getCard(“Darkrai”);
+        trade2.list2.add(owner.getInventory().getCard(“Mewtwo”);
+
+        // User clicks the delete trade button or back
+        // button to stop the trade they are making.
+        trade2.destructor();
+
+        assertNull(trade2);
+    }
+
+// US04.07.01
+// If the owner of a trade accepts, both parties are email relevant trace information,
+// as well as the owner will supply comments for how to continue with the trade.
+
+    void testUS040701 () {
+        User borrower = new User(currentUser.Name, “Canada”);
+        User owner = new User(“Mr. Bean”, “Canada”);
+
+        // Borrower adds cards with the “plus” button.
+        Trade trade = new Trade(borrower, owner);
+        trade.list1.add(borrower.getInventory().getCard(“Darkrai”);
+        trade.list2.add(owner.getInventory().getCard(“Mewtwo”);
+
+        // Send trade offer button clicked.
+        trade.sendTrade(owner);
+        trade.setNotification(owner);
+
+        // Owner clicks the accept button. An input box
+        // will be displayed for extra comments.
+        String extraComments = inputTextView.text;
+        Email emailBorrower = new Email(borrower, extraComments);
+        Email emailOwner = new Email(owner, “”);
+
+        emailBorrower.send();
+        emailOwner.send();
+
+        assertNotNull(extraComments);
+        assertEquals(emailOwner.status() ,“SENT”);
+        assertEquals(emailBorrower.status(), “SENT”);
+    }
+
+    Trade With Friends:
+
+    //Use Case Name: Users List Of Trades
+//Participating Actors: User, Trade
+//Goal: The user can view a list of their trades and check to see if they
+    were a borrower or an owner in each trade.
+    //Trigger: User clicks the trades menu option
+//Precondition: User is in the main menu
+//Postcondition: User has a list of trades
+    and the friend has accepted the trade.
+//Related User Stories: US04.08.01 - US04.09.01
+
+//Basic Flow
+// 1) User clicks the trade menu option
+// 2) A scrollable list of trades opens.
+// 3) User selects a trade that they were in.
+// 4) User checks to see if they were a borrower or an owner in that trade
+// 5) User exits the current trade
+
+//Exceptions
+// 5) User presses the back button
+// 5.1) The list of trades will be closed and the use case will be exited.
+
+// US04.08.01
+// Owners and Borrowers can view all past trades with themselves.
+
+    void testUS040801 () {
+        User borrower = new User(currentUser.Name, “Canada”);
+        User owner = new User(“Mr. Bean”, “Canada”);
+
+        // Borrower adds cards with the “plus” button.
+        Trade trade1 = new Trade(borrower, owner);
+        Trade trade2 = new Trade(borrower, owner);
+
+        // Send trade offer button clicked.
+        trade1.sendTrade(owner);
+        trade2.sendTrade(owner);
+
+        // Owner accepts trade1 and leaves trade2 alone
+        // User exits and checks there trades
+        List<Trade> ownerTrades = owner.getTrades()
+        List<Trade> borrowerTrades = owner.getTrades()
+
+        assertTrue(ownerTrades.contains(trade1));
+        assertTrue(ownerTrades.contains(trade2));
+        assertTrue(borrowerTrades.contains(trade1));
+        assertTrue(borrowerTrades.contains(trade2));
+    }
+
+// US04.09.01
+// Owners and Borrowers can view all past trades with themselves as
+// either a borrower or an owner.
+
+    void testUS040901 () {
+        User user1 = new User(currentUser.Name, “Canada”);
+        User user2 = new User(“Mr. Bean”, “Canada”);
+
+        // Borrower adds cards with the “plus” button.
+        Trade trade1 = new Trade(user1, user2); //user 1 sends a trade to user 2
+        Trade trade2 = new Trade(user2, user1); //user 2 sends a trade to user 1
+
+        // Send trade offer button clicked.
+        trade1.sendTrade(user1); //is the user parameter necessary? we already specified in "new Trade(...,...)"
+        trade2.sendTrade(user2);
+
+        // Owner accepts trade1 and leaves trade2 alone
+        // User exits and checks there trades
+        List<Trade> user1Trades = user1.getTrades()
+        List<Trade> user2Trades = user2.getTrades()
+
+        assertTrue(user1Trades.get(trade1).isOwner()); //make sure user 1 owns trade 1
+        assertTrue(user2Trades.get(trade1).isBorrower()); //make sure user 2 is borrowing trade 1
+        assertTrue(user1Trades.get(trade2).isBorrower()); //make sure user 1 is borrowing trade 1
+        assertTrue(user2Trades.get(trade2).isOwner()); //make sure user 2 owns trade 1
+
+        assertTrue(!user1Trades.get(trade2).isOwner()); //make sure user 1 doesnt own trade 2
+        assertTrue(!user2Trades.get(trade2).isBorrower()); //make sure user 2 doesnt borrow trade 2
+
+        //Viewing trades and separating them will be done in UI.
+    }
+
+
 }
--- ReCarded/app/src/androidTest/java/com/sherpasteven/sscte/UseCase6Test.java
+++ ReCarded/app/src/androidTest/java/com/sherpasteven/sscte/UseCase6Test.java
@@ -10,4 +10,164 @@ public class UseCase6Test extends ApplicationTestCase<Application> {
     public UseCase6Test(Class<Application> applicationClass) {
         super(applicationClass);
     }
+
+    //Use Case Name: User uploads and deletes photos of their cards
+//Participating Actors: Owner, Other Users
+//Goal: User can upload and delete pictures of cards they can own.
+//Trigger: User selects edit photos on a card in thier inventory
+//Precondition: if a user want to upload a card image they have it available.
+//Postcondition: On success, pitures are uploaded for owner and other users to see on upload. On delete, owner and other users cannot see deleted picture
+//Related User Stories: US06.01.01 - US06.04.01
+
+//Basic Flow
+// 1.) Owner selects a card from thier inventory to add an image
+// 2.) Owner initiates add dialog and a picture browsing routine is brought up
+// 3.) Owner selects image(s) to be uploaded to card display
+// 4.) Owner and Other Users can now see uploaded images of card
+// 5.) Owner selects a card from thier inventory to delete an image from it
+// 6.) Owner selects image(s) from dialog to remove from card
+// 7.) Image(s) removed can no longer be seen by Owner or Other Users
+
+//Exceptions
+// 3.1.) If image is over 65536 bytes
+// 3.2.) System displays an error describing limitation
+// 3.3.) System returns to step 3
+// 7.1.) Other Users have deleted image downloaded locally (See US06.05.01, US09.03.01, and US10.01.01)
+// 7.2.) Locally downloaded copies of an image on Other User's devices are deleted once they reconnect to the service if they were offline.
+
+
+
+    US06.01.01
+//Claimant attaches photograph to an item
+
+    void testUS030201(){
+        String name = "Charizard";
+        Integer quantity = 2;
+        Quality quality = new Quality(73);
+        String catagory = "Pokemon";
+        String series = "series";
+        boolean tradable = true;
+        String comments = "Gently bent edge, 100HP";
+        Card card = new Card(name, quantity, quality, catagory, series, tradable, comments);
+
+        //user navigates in the create a card GUI to the "Add images"
+        // section and can add images from camera or take a new one
+        Image firstImage = new Image("myImageUrl");
+        Image secondImage = new Image("myImageUrl");
+        card.addImage(firstImage);
+        card.addImage(secondImage);
+        List<Image> cardImages = card.getImages();
+        assertTrue(cardImages.contains(firstImage));
+        assertTrue(cardImages.contains(secondImage));
+    }
+
+    US06.02.01
+//Owner has an Item
+//Owner gets photos attached to items
+
+    void testUS060201(){
+        String name = "Charizard";
+        Integer quantity = 2;
+        Quality quality = new Quality(73);
+        String catagory = "Pokemon";
+        String series = "series";
+        boolean tradable = true;
+        String comments = "Gently bent edge, 100HP";
+        Card card = new Card(name, quantity, quality, catagory, series, tradable, comments);
+
+
+        //user navigates in the create a card GUI to the "Add images"
+        // section and can add images from camera or take a new one
+        Image firstImage = new Image("myImageUrl");
+        Image secondImage = new Image("myImageUrl");
+        card.addImage(firstImage);
+        card.addImage(secondImage);
+        List<Image> cardImages = card.getImages();
+        assertTrue(cardImages.contains(firstImage));
+        assertTrue(cardImages.contains(secondImage));
+    }
+
+    US06.03.01
+//User has an Item
+//User deletes image from item
+
+    void testUS060301(){
+        String name = "Charizard";
+        Integer quantity = 2;
+        Quality quality = new Quality(73);
+        String catagory = "Pokemon";
+        String series = "series";
+        boolean tradable = true;
+        String comments = "Gently bent edge, 100HP";
+        Card card = new Card(name, quantity, quality, catagory, series, tradable, comments);
+        Image firstImage = new Image("myImageUrl");
+        Image secondImage = new Image("myImageUrl");
+        card.addImage(firstImage);
+        card.addImage(secondImage);
+
+        List<Image> cardImages = card.getImages();
+        assertTrue(cardImages.contains(firstImage));
+        assertTrue(cardImages.contains(secondImage));
+
+        //user from the edit inventory item GUI navigates to the
+        //images view and can remove, add or rearrange images
+        card.deleteImage(firstImage);
+        List<Image> cardImages = card.getImages();
+        assertTrue(!cardImages.contains(firstImage));
+        assertTrue(cardImages.contains(secondImage));
+    }
+
+    US06.04.01
+//User adds an Image larger than 65536 bytes
+//Card notifies that it cannot add a image file of that size
+
+    void testUS060401{
+        Boolean exceptionThrown = false;
+        Image image = new Image("largeImageUrl");
+        try {
+            String name = "Charizard";
+            Integer quantity = 2;
+            Quality quality = new Quality(73);
+            String catagory = "Pokemon";
+            String series = "series";
+            boolean tradable = true;
+            String comments = "Gently bent edge, 100HP";
+
+            Card card = new Card(name, quantity, quality, catagory, series, tradable, comments);
+            card.addImage(image); //this should throw IOexption
+
+        } catch (IOexception ex) {
+            exceptionThrown = true;
+        }
+        assertTrue(exceptionThrown);
+    }
+
+
+//Use Case Name: Downloading Specific Images
+//Participating Actors: Borrower, Owner
+//Goal: Borrower can select specific images to download even if they have downloads disabled
+//Trigger: Borrower is browsing through a photo album of a Owner's card
+//Precondition: Borrower must select image to be downloaded
+//Postcondition: On success, pictures that were selected to be downloaded will be available offline
+//Related User Stories: US06.05.01
+
+//Basic Flow
+// 1.) Borrower is browsing through images of one of Owner's cards, but has local downloads turned off (See US10.01.01)
+// 2.) Borrower select an image in the album
+// 3.) On first time image selection, system prompts message about being able to download images
+// 4.) Image is highlighted or watermarked to notify that image is locally cached
+// 5.) Borrower can disconnect from internet and still browse specific images that they downloaded
+
+    US06.05.01
+//Borrower has photo downloads disabled
+//Borrower manually downloads item images
+
+    void testUS600501{
+        User user = GetMockUser();
+        Inventory inventory = owner.getInventory();
+        Card card = inventory.searchByText("Black Lotus");
+        List<Image> images = card.getImages();
+        //saveLocalResource(int ownerHashCode, string key, object value);
+        saveLocalResource(card.hashCode, "images", images);
+    }
 }
