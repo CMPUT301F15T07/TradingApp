a4fdb85e43f928eea5c56398aa273ff97d78da50
Anson Li <ansonli.94@gmail.com>
2015-10-31 15:01:09 -0600

    Test activities for setting, add friend and change profile

---
+++ ReCarded/app/src/main/java/com/sherpasteven/sscte/SettingsActivity.java
@@ -0,0 +1,48 @@
+package com.sherpasteven.sscte;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.sherpasteven.sscte.Models.Profile;
+import com.sherpasteven.sscte.Views.IView;
+
+public class SettingsActivity extends AppCompatActivity implements IView<Profile>{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_settings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent1 = new Intent(this, SettingsActivity.class);
+            this.startActivity(intent1);
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void Update(Profile profile) {
+
+    }
+}
