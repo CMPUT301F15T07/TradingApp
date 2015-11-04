8e53f60abea94062f0e6a46aa7ceb8031d1ad0bf
Anson Li <ansonli.94@gmail.com>
2015-11-04 12:47:28 -0700

    Profile initial image loading support (working!)

--- ReCarded/app/src/main/java/com/sherpasteven/sscte/ProfileActivity.java
+++ ReCarded/app/src/main/java/com/sherpasteven/sscte/ProfileActivity.java
@@ -1,19 +1,72 @@
 package com.sherpasteven.sscte;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.sherpasteven.sscte.Models.Profile;
 import com.sherpasteven.sscte.Views.IView;
 
+import java.io.File;
+
 public class ProfileActivity extends AppCompatActivity implements IView<Profile>{
 
+    private static int RESULT_LOAD_IMAGE = 1;
+    // code graciously provided at http://viralpatel.net/blogs/pick-image-from-galary-android-app/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        ImageButton buttonLoadImage = (ImageButton) findViewById(R.id.btnProfileImage);
+        buttonLoadImage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            ImageView imageView = (ImageView) findViewById(R.id.profile_image);
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+        }
+
+
     }
 
     @Override
@@ -42,4 +95,6 @@ public class ProfileActivity extends AppCompatActivity implements IView<Profile>
     public void Update(Profile profile) {
         
     }
+
+
 }
