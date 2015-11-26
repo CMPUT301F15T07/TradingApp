ab558ae85929623eec6adf5ece185e5b2ba59db0
Anson Li <ansonli.94@gmail.com>
2015-11-26 12:13:36 -0700

    View Trade initial development

---
+++ ReCarded/app/src/main/java/com/sherpasteven/ViewTradeActivity.java
@@ -0,0 +1,15 @@
+package com.sherpasteven;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sherpasteven.sscte.R;
+
+public class ViewTradeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_trade);
+    }
+}
