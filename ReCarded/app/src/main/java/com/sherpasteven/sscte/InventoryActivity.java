66deb7247f26529ce54a6eb26049c61532ea1a61
Anson Li <ansonli.94@gmail.com>
2015-11-29 11:54:43 -0700

    ui changes min

--- ReCarded/app/src/main/java/com/sherpasteven/sscte/InventoryActivity.java
+++ ReCarded/app/src/main/java/com/sherpasteven/sscte/InventoryActivity.java
@@ -95,12 +95,9 @@ public class InventoryActivity extends ActionBarActivity implements IView<Model>
         toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
 
-        /*
         if (android.os.Build.VERSION.SDK_INT >= 21) { // attempt for conditional run
             changeToolbarColor();
         }
-        */
-
 
         // Creating The ViewPagerAdapter and Passing Fragment Manager, Titles fot the Tabs and Number Of Tabs.
         adapter =  new ViewPagerAdapter(getSupportFragmentManager(),Titles,Numboftabs,currentuser);
